5df32a8fa17b3a637ccdb44443e5d0b2d58dae8b
user <user@10.0.0.10>
2020-10-27 16:08:51 +0200

    added another statewment

--- src/com/testing/Test2.java
+++ src/com/testing/Test2.java
@@ -3,5 +3,6 @@ package com.testing;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("This i have added as  a engineer");
+		System.out.println("Here is another statement as asked by the lead engineer");
 	}
 }
